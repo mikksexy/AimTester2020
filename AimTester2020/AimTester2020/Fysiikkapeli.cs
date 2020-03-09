2fcda78e8efafc2876a8ca3f1c9b28b5cee33331
mikksexy <sami.ea.mikkola@student.jyu.fi>
2020-03-09 20:50:19 +0200

    Erät toimivat, to do: laskurit, valikot, epäonnistuminen

--- AimTester2020/AimTester2020/Fysiikkapeli.cs
+++ AimTester2020/AimTester2020/Fysiikkapeli.cs
@@ -10,18 +10,45 @@ public class AimTester2020 : PhysicsGame
 
     GameObject kursori;
     GameObject vihu;
-    //Timer ajastin;
+    Timer ajastin;
+    int tuhottuja = 0; // Tuhottujen vihujen määrä
+    int eraNumero = 0;
+    const int pelinLeveys = 800;
+    const int pelinKorkeus = 600;
     public override void Begin()
     {
-        LuoKursori();
-        AsetaOhjaimet();
-        NopeutaEraa();
-
-        SetWindowSize(1024, 768, false);
+        SetWindowSize(pelinLeveys, pelinKorkeus, false);
 
         Level.Background.Color = Color.Black;
 
         Camera.ZoomToLevel();
+
+        AloitaPeli();
+
+
+    }
+
+
+    public void AloitaPeli()
+    {
+        LuoKursori();
+
+        ajastin = new Timer();
+        Era();
+        ajastin.Timeout += LuoVihu;
+        ajastin.Start();
+
+        Mouse.Listen(MouseButton.Left, ButtonState.Pressed, delegate ()
+        {
+            if (Vector.Distance(kursori.Position, vihu.Position) < vihu.Width / 2) // Jos kursori ja vihu ovat päällekkäin
+            {
+                vihu.Destroy();
+                tuhottuja++;
+                Era(); // Kutsuu ohjelmaa, jotta voidaan tarkistaa tarvitseeko vihuja spawnata nopeampaa
+            }
+        }, "Tuhoaa olion");
+
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
     
 
@@ -31,14 +58,11 @@ public class AimTester2020 : PhysicsGame
         kursori.Shape = Shape.Triangle;
         kursori.Color = Color.Green;
         Add(kursori);
-        Mouse.ListenMovement(0, LiikutaKursoria, "Liikuttaa hiirtä");
+        Mouse.ListenMovement(0, delegate()
+        {
+            kursori.Position = Mouse.PositionOnWorld;
+        }, "Liikuttaa hiirtä");
         kursori.Position = Mouse.PositionOnScreen;
-    }
-
-
-    void LiikutaKursoria()
-    {
-        kursori.Position = Mouse.PositionOnWorld;
     }
 
 
@@ -47,42 +71,33 @@ public class AimTester2020 : PhysicsGame
         vihu = new GameObject(50, 50);
         vihu.Shape = Shape.Circle;
         vihu.Color = Color.Yellow;
-        vihu.X = RandomGen.NextDouble(-512, 512);
-        vihu.Y = RandomGen.NextDouble(-384, 384);
+        vihu.X = RandomGen.NextDouble(-pelinLeveys / 2, pelinLeveys / 2);
+        vihu.Y = RandomGen.NextDouble(-pelinKorkeus / 2, pelinKorkeus / 2);
+        //vihu.Position = RandomGen.NextVector(pelinLeveys, pelinKorkeus);
         Add(vihu);
     }
 
-
-    public void NopeutaEraa()
+    /// <summary>
+    /// Aliohjelma tarkistaa onko eriä jäljellä ja onko erä suoritettu eli 10 vihua tuhottua.
+    /// </summary>
+    public void Era()
     {
-        double[] era = { 2.0, 6.0, 5.0, 4.0, 3.0, 2.5, 2.0, 1.5, 1.2, 1.0 };
-        for (int i = 0; i < era.Length; i++)
+        double[] era = { 2.0, 6.0, 1.0, 4.0, 3.0, 2.5, 2.0, 1.5, 1.2, 1.0 };
+        if (eraNumero == era.Length) Lopetus(); // Jos kaikki erät suoritettu kutsuu Lopetus -aliohjelmaa
+        if (tuhottuja % 10 == 0) // Jos erä suoritettu 
         {
-            /*ajastin = new Timer();
-            ajastin.Interval = era[i];
-            ajastin.Timeout += LuoVihu;
-            ajastin.Start();
-            ajastin.Reset();*/
-            Timer.CreateAndStart(era[i], LuoVihu);
+            ajastin.Interval = era[eraNumero]; // hakee taulukosta uuden eränopeuden
+            ajastin.Reset();
+            ajastin.Start(); // käynnistää ajastimen uudella nopeudella
+            eraNumero++; // päivittää eränumeron
         }
     }
 
 
-    void AsetaOhjaimet()
+    public void Lopetus()
     {
-
-        Mouse.Listen(MouseButton.Left, ButtonState.Pressed, Tuhoa, "Tuhoaa olion");
-
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
-    }
-
-
-    void Tuhoa()
-    {
-        if (Vector.Distance(kursori.Position, vihu.Position) < vihu.Width / 2)
-            // on päällekkäistä aluetta
-            {
-            vihu.Destroy();
-            }
+        ajastin.Reset();
+        Label voititPelin = new Label("Voitit pelin!");
+        Add(voititPelin);
     }
 }
