d44bdf44a3e88afc72b6ddb5486e53e3b19cf08b
mikksexy <sami.ea.mikkola@student.jyu.fi>
2020-04-15 12:00:32 +0300

    Kenties valmis versio

--- AimTester2020/AimTester2020/Fysiikkapeli.cs
+++ AimTester2020/AimTester2020/Fysiikkapeli.cs
@@ -7,47 +7,79 @@ using Jypeli.Widgets;
 
 
 /// @author  Sami Mikkola
-/// @version 11.03.2020
+/// @version 15.04.2020
 /// <summary>
-/// Peli, jossa vihuja (palloja) tuhotaan klikkailemalla kursorilla niiden päältä mahdollisimman nopeaa.
+/// Peli, jossa vihuja (palloja) tuhotaan klikkailemalla kursorilla niiden päältä mahdollisimman nopeasti.
 /// </summary>
+/// TODO: funktio + silmukka, ks: https://tim.jyu.fi/view/kurssit/tie/ohj1/2020k/demot/demo9-tutkimus?answerNumber=3&b=qNc4xpHaic24&size=1&task=D9T1&user=mikksexy
 public class AimTester2020 : PhysicsGame
 {
-
     private GameObject kursori;
     private GameObject vihu;
     private Timer vihuAjastin;
     private IntMeter eraNumero;
     private IntMeter tuhottuja; // Tuhottujen vihujen määrä
     private IntMeter vihuMaara;
-    private const int pelinLeveys = 800;
-    private const int pelinKorkeus = 600;
+
+    private const int PELIN_LEVEYS = 800;
+    private const int PELIN_KORKEUS = 600;
+
+
+    /// <summary>
+    /// Aloitusruutu
+    /// </summary>
     public override void Begin()
     {
-        SetWindowSize(pelinLeveys, pelinKorkeus, false);
-
+        Valikot(1);
         Level.Background.Color = Color.Black;
-
-        MultiSelectWindow alkuValikko = new MultiSelectWindow("AimTester2020", "Aloita peli", "Lopeta");
-        Add(alkuValikko);
-        alkuValikko.AddItemHandler(0, AloitaPeli);
-        alkuValikko.AddItemHandler(1, ConfirmExit);
-
-        vihuMaara = new IntMeter(0);
-
+        SetWindowSize(PELIN_LEVEYS, PELIN_KORKEUS, false);
     }
+
+
+    /// <summary>
+    /// Käynnistää halutun valikon
+    /// </summary>
+    /// <param name="caseNumero">case numero</param>
+    private void Valikot(int caseNumero)
+    {
+        switch (caseNumero)
+        {
+            case 1:
+                MultiSelectWindow alkuValikko = new MultiSelectWindow("AimTester2020", "Aloita peli", "Lopeta");
+                Add(alkuValikko);
+                alkuValikko.AddItemHandler(0, AloitaPeli);
+                alkuValikko.AddItemHandler(1, ConfirmExit);
+                break;
+
+            case 2:
+                ClearAll();
+                MultiSelectWindow havisitPelin = new MultiSelectWindow("HÄHÄÄ!\nHävisit pelin", "Pelaa uudestaan", "Lopeta");
+                Add(havisitPelin);
+                havisitPelin.AddItemHandler(0, AloitaPeli);
+                havisitPelin.AddItemHandler(1, Exit);
+                break;
+
+            case 3:
+                ClearAll();
+                MultiSelectWindow lopetusValikko = new MultiSelectWindow("ONNITTELUT!\nLäpäisit kohdistamistestin", "Pelaa uudestaan", "Lopeta");
+                Add(lopetusValikko);
+                lopetusValikko.AddItemHandler(0, AloitaPeli);
+                lopetusValikko.AddItemHandler(1, Exit);
+                break;
+        }
+    }
+
 
     /// <summary>
     /// Aliohjelma aloittaa pelin eli aloittaa luo kursorin ja aloittaa vihujen spawnausajastimen
     /// </summary>
-    public void AloitaPeli()
+    private void AloitaPeli()
     {
+        ClearAll();
+
         Camera.ZoomToLevel();
-        while (vihuMaara != 0)
-        {
-            vihu.Destroy();
-            vihuMaara.Value -= 1;
-        }
+
+        vihuMaara = new IntMeter(0);
 
         LuoKursori();
 
@@ -61,10 +93,11 @@ public class AimTester2020 : PhysicsGame
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
     
+
     /// <summary>
     /// Aliohjelma luo kursorin ja määrittää samalla hiiren toiminnot
     /// </summary>
-    public void LuoKursori()
+    private void LuoKursori()
     {
         kursori = new GameObject(50, 50);
         kursori.Shape = Shape.Triangle;
@@ -75,7 +108,6 @@ public class AimTester2020 : PhysicsGame
         {
             kursori.Position = Mouse.PositionOnWorld;
         }, "Hiiren liike liikuttaa kursoria");
-        //kursori.Position = Mouse.PositionOnScreen;
 
         Mouse.Listen(MouseButton.Left, ButtonState.Pressed, delegate ()
         {
@@ -89,21 +121,22 @@ public class AimTester2020 : PhysicsGame
         }, "Hiiren vasenta painettaessa tuhoaa olion, jos ehto täyttyy");
     }
 
+
     /// <summary>
     /// Luo eriä laskevan laskurin
     /// </summary>
-    public void LuoEralaskuri()
+    private void LuoEralaskuri()
     {
         eraNumero = new IntMeter(0);
 
-        Label pisteNaytto = new Label();
-        pisteNaytto.X = Screen.Left + 100;
-        pisteNaytto.Y = Screen.Top - 100;
-        pisteNaytto.TextColor = Color.Black;
-        pisteNaytto.Color = Color.White;
-
-        pisteNaytto.BindTo(eraNumero);
-        Add(pisteNaytto);
+        Label eralaskuri = new Label();
+        eralaskuri.X = Screen.Left + 100;
+        eralaskuri.Y = Screen.Top - 100;
+        eralaskuri.TextColor = Color.Black;
+        eralaskuri.Color = Color.White;
+        eralaskuri.BindTo(eraNumero);
+        eralaskuri.IntFormatString = "Erä {0:D1}";
+        Add(eralaskuri);
     }
 
 
@@ -111,59 +144,38 @@ public class AimTester2020 : PhysicsGame
     /// Aliohjelma luo vihun eli objektin, joka pelissä halutaan tuhota.
     /// Samalla määrittää vihun ominaisuudet
     /// </summary>
-    public void LuoVihu()
+    private void LuoVihu()
     {
-        if (vihuMaara > 0)
+        if (vihuMaara > 0) // Jos kentällä on vihuja enemmän kuin yksi, peli hävitään.
         {
-            MultiSelectWindow havisitPelin = new MultiSelectWindow("HÄHÄÄ!\nHävisit pelin", "Pelaa uudestaan", "Lopeta");
-            Lopetus(havisitPelin);
+            Valikot(2);
         }
         vihu = new GameObject(50, 50);
         vihu.Shape = Shape.Circle;
         vihu.Color = Color.Yellow;
-        vihu.X = RandomGen.NextDouble(-pelinLeveys / 2, pelinLeveys / 2);
-        vihu.Y = RandomGen.NextDouble(-pelinKorkeus / 2, pelinKorkeus / 2);
+        vihu.X = RandomGen.NextDouble(-PELIN_LEVEYS / 2, PELIN_LEVEYS / 2);
+        vihu.Y = RandomGen.NextDouble(-PELIN_KORKEUS / 2, PELIN_KORKEUS / 2);
         Add(vihu);
         vihuMaara.Value += 1;
     }
 
+
     /// <summary>
     /// Aliohjelma tarkistaa onko eriä jäljellä ja onko erä suoritettu eli 10 vihua tuhottuna.
     /// </summary>
-    public void Era()
+    private void Era()
     {
-        double[] eraNopeus = { /*2.0, 6.0, 1.0, 4.0, 3.0, 2.5, */2.0, 1.5, 1.2, 1.0 };
-        if (tuhottuja % 10 == 0) // Jos erä suoritettu 
+        double[] eraNopeus = { 2.0, 1.5, 1.2, 1.0 };
+        if (tuhottuja % 10 == 0) // Jos erä suoritettu
         {
             vihuAjastin.Reset();
             vihuAjastin.Interval = eraNopeus[eraNumero]; // hakee taulukosta uuden eränopeuden
             vihuAjastin.Start(); // käynnistää ajastimen uudella nopeudella
             eraNumero.Value += 1; // päivittää eränumeron
         }
-        if (eraNumero == eraNopeus.Length)
+        if (eraNumero == eraNopeus.Length) // Jos eränumero on sama kuin määritettyjen erien määrä, peli voitetaan.
         {
-            MultiSelectWindow lopetusValikko = new MultiSelectWindow("ONNITTELUT!\nLäpäisit kohdistamistestin", "Pelaa uudestaan", "Lopeta");
-            Lopetus(lopetusValikko); // Jos kaikki erät suoritettu kutsuu Lopetus -aliohjelmaa
+            Valikot(3);
         }
-
-    }
-
-    /// <summary>
-    /// Aliohjelma lopettaa vihujen spawnaamisen ja avaa valikon,
-    /// jossa onnitellaan pelin läpäisemisestä sekä tarjotaan
-    /// mahdollisuus pelata uudestaan tai lopettaa peli.
-    /// </summary>
-    public void Lopetus(MultiSelectWindow valikko)
-    {
-        vihuAjastin.Stop(); // Pysäyttää vihujen spawnaamisen
-        vihu.Destroy();
-        vihuMaara.Value -= 1;
-        kursori.Destroy();
-        tuhottuja.Reset();
-        eraNumero.Reset();
-        ClearAll();
-        Add(valikko);
-        valikko.AddItemHandler(0, AloitaPeli);
-        valikko.AddItemHandler(1, Exit);
     }
 }
